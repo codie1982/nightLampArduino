841560fcea496b80f489e80ce582d7b97eea5597
Engin EROL <engin_erol@hotmail.com>
2024-03-21 21:54:41 +0300

    first commit

---
+++ nightLamp01.ino
@@ -0,0 +1,25 @@
+#include <ESP8266WebServer-impl.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
+#include <Parsing-impl.h>
+#include <Uri.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+
+
+
+#define REDPIN D1
+#define BLUEPIN D2
+#define GREENPIN D3
+#define OPENTIME 10L
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
